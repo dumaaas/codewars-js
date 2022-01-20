0ac5ccfe417bf91da519dbad6ccb753ce354e924
Marko Dumnic <marko.dumnic@bild-studio.net>
2022-01-20 17:33:40 +0100

    changed scrollbar style

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,16 +7,23 @@
 </template>
 
 <script>
-
-
 export default {
-  components: {
-
-  },
+  components: {},
 }
 </script>
 
 <style>
+body::-webkit-scrollbar {
+  width: 12px; /* width of the entire scrollbar */
+}
+body::-webkit-scrollbar-track {
+  background: #1F1F1F; /* color of the tracking area */
+}
+body::-webkit-scrollbar-thumb {
+  background-color: #bb86fc; /* color of the scroll thumb */
+  border-radius: 20px; /* roundness of the scroll thumb */
+  border: 1px solid #121212; /* creates padding around scroll thumb */
+}
 html {
   font-family: 'Mulish', -apple-system, BlinkMacSystemFont, 'Arial', Roboto,
     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
